59d1c2e4633929def0ffa489bca931f1d4a91a31
Arya Suwandika <aryaswan17@gmail.com>
2023-11-06 17:46:43 +0800

    order beta version

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//to reset selected menu
+window.addEventListener("load", checkbox);
+
+var jam = document.getElementById("jam")
+var sekarang = document.getElementById("sekarang");
+
 function tanya() {
 	var tanda = document.querySelector('input[name="tanya"]:checked').value;
 	if (tanda == "Menu") {
@@ -7,15 +13,28 @@ function tanya() {
 	}
 }
 
+// order prototype
 function pesan() {
+	var nama = document.getElementById("nama").value;
+	var lokasi = document.getElementById("lokasi").value;
+	var waktu = document.querySelector("input[name='waktu']:checked").value
+	if (waktu == "Sekarang") {
+		waktu = "Sekarang"
+	} else if (waktu == "Jam") {
+		waktu = "Jam%20" + document.getElementById("txtjam").value;
+	}
 	var pesanan = document.querySelectorAll('input[name="order"]:checked');
-	var message = "https://wa.me/6281770219886?text="
+	// var message = "https://wa.me/6281770219886?text=Nama%20:%20" + nama + "\nDikirim%20ke%20:%20" + lokasi + "\nKapan%20dikirim%20:%20" + waktu + "\nOrder%20:%20"
+	var message = "https://wa.me/6282158566320?text=Nama%20:%20" + nama + "%0ADikirim%20ke%20:%20" + lokasi + "%0AKapan%20dikirim%20:%20" + waktu + "%0AOrder%20:%20"
 	pesanan.forEach((checkbox) => {
-		console.log(checkbox.value)
+		var jumlah = "txt" + checkbox.id;
+		message = message + "%0A" + checkbox.value + "%20" + document.getElementById(jumlah).value + "x"
 	});
+	window.location.href = message;
 	checkbox();
 }
 
+// showing fields of each menu
 function showfield(checkbox) {
 	var box = checkbox.id;
 	var field = 'txt' + box;
@@ -27,11 +46,20 @@ function showfield(checkbox) {
 
 }
 
-window.addEventListener("load", checkbox);
-
+//reset checked menu
 function checkbox() {
 	var pesanan = document.querySelectorAll('input[name="order"]:checked');
 	pesanan.forEach((box) => {
 		box.checked = false;
 	})
 }
+
+// checks if jam is selected, if selected will show field for when to deliver
+jam.addEventListener("click", function check() {
+	document.getElementById("txtjam").style.display = 'block'
+})
+// else it will disappear and reset
+sekarang.addEventListener("click", function check() {
+	document.getElementById("txtjam").style.display = 'none'
+	document.getElementById("txtjam").value = ''
+})
