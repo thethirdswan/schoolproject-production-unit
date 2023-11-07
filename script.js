1d2a2da3da9ff2e050c0ad8f55503e7ff6f4aacb
Arya Suwandika <aryaswan17@gmail.com>
2023-11-07 17:19:39 +0800

    finishing order pop up, footer

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 //to reset selected menu
-window.addEventListener("load", checkbox);
+// window.addEventListener("load", checkbox);
+window.addEventListener("load", identity);
+
+if (localStorage.getItem("Nama") == null) {
+	localStorage.setItem("Nama", "")
+}
+
+if (localStorage.getItem("Lokasi") == null) {
+	localStorage.setItem("Lokasi", "")
+}
 
 var jam = document.getElementById("jam")
 var sekarang = document.getElementById("sekarang");
 
+//for asking
 function tanya() {
 	var tanda = document.querySelector('input[name="tanya"]:checked').value;
 	if (tanda == "Menu") {
-		window.location.href = "https://wa.me/6281770219886?text=Menu%20hari%20ini%20apa%20aja,%20kak?"
+		window.location.href = "https://wa.me/6281770219886?text=Menu%20hari%20ini%20apa%20aja,%20kak?";
 	} else {
-		window.location.href = "https://wa.me/6281770219886?text=Bisa%20ngeprint%20ngga,%20kak?"
+		window.location.href = "https://wa.me/6281770219886?text=Bisa%20ngeprint%20ngga,%20kak?";
 	}
+}
+
+function identity() {
+	var nama = document.getElementById("nama")
+	var lokasi = document.getElementById("lokasi")
+	var storagenama = localStorage.getItem("Nama")
+	var storagelokasi = localStorage.getItem("Lokasi")
+	nama.value = storagenama
+	lokasi.value = storagelokasi
 }
 
 // order prototype
@@ -18,20 +37,31 @@ function pesan() {
 	var nama = document.getElementById("nama").value;
 	var lokasi = document.getElementById("lokasi").value;
 	var waktu = document.querySelector("input[name='waktu']:checked").value
+	var rememberidentity = document.getElementById("rememberidentity");
 	if (waktu == "Sekarang") {
 		waktu = "Sekarang"
 	} else if (waktu == "Jam") {
 		waktu = "Jam%20" + document.getElementById("txtjam").value;
 	}
 	var pesanan = document.querySelectorAll('input[name="order"]:checked');
-	// var message = "https://wa.me/6281770219886?text=Nama%20:%20" + nama + "\nDikirim%20ke%20:%20" + lokasi + "\nKapan%20dikirim%20:%20" + waktu + "\nOrder%20:%20"
-	var message = "https://wa.me/6282158566320?text=Nama%20:%20" + nama + "%0ADikirim%20ke%20:%20" + lokasi + "%0AKapan%20dikirim%20:%20" + waktu + "%0AOrder%20:%20"
-	pesanan.forEach((checkbox) => {
-		var jumlah = "txt" + checkbox.id;
-		message = message + "%0A" + checkbox.value + "%20" + document.getElementById(jumlah).value + "x"
-	});
-	window.location.href = message;
-	checkbox();
+	if (nama == '' || lokasi == '') {
+		window.alert("Anda belum mengisi identitas!")
+	} else if (pesanan.length == 0) {
+		window.alert("Anda belum memilih menu untuk dipesan!")
+	} else {
+		// var message = "https://wa.me/6281770219886?text=Nama%20:%20" + nama + "\nDikirim%20ke%20:%20" + lokasi + "\nKapan%20dikirim%20:%20" + waktu + "\nOrder%20:%20"
+		var message = "https://wa.me/6282158566320?text=Nama%20:%20" + nama + "%0ADikirim%20ke%20:%20" + lokasi + "%0AKapan%20dikirim%20:%20" + waktu + "%0AOrder%20:%20"
+		if (rememberidentity.checked == true) {
+			localStorage.setItem("Nama", nama)
+			localStorage.setItem("Lokasi", lokasi)
+		}
+		pesanan.forEach((checkbox) => {
+			var jumlah = "txt" + checkbox.id;
+			message = message + "%0A" + checkbox.value + "%20" + document.getElementById(jumlah).value + "x"
+		});
+		checkbox();
+		window.location.href = message;
+	}
 }
 
 // showing fields of each menu
